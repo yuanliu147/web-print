9b6c016f4efb7b4844513159f2a23a35dcee5178
liuyuan <liuyuan@movee.cn>
2025-04-15 22:15:23 +0800

    feat: container 拖拽实现

--- src/app/editor/components/EditorArea/components/BoxContainer.tsx
+++ src/app/editor/components/EditorArea/components/BoxContainer.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useContext } from 'react'
+import React, { useContext, useRef } from 'react'
 import { StoreContext } from '@/app/editor/StoreContext'
 
 import './styles.scss'
@@ -13,24 +13,68 @@ export default function BoxContainer({
 	children,
 	id = Date.now() + '',
 }: React.PropsWithChildren<BoxContainerProps>) {
-	const containerRef = React.useRef<HTMLDivElement>(null)
+	const containerRef = useRef<HTMLDivElement>(null)
 	const { globalData, setGlobalData } = useContext(StoreContext)
+
+	const currIsActive = id === globalData.activeElement?.id
 
 	return (
 		<div
+			id={id}
 			ref={containerRef}
-			className={`boxWrap ${id === globalData.activeElement?.id ? 'active' : ''}`}
+			className={`boxWrap ${currIsActive ? 'active' : ''}`}
 			onClick={() => {
 				setGlobalData({
 					...globalData,
 					activeElement: {
 						id,
-						width: containerRef.current?.offsetWidth || 0,
-						height: containerRef.current?.offsetHeight || 0,
-						offsetLeft: containerRef.current?.offsetLeft || 0,
-						offsetTop: containerRef.current?.offsetTop || 0,
+						width: containerRef.current!.offsetWidth,
+						height: containerRef.current!.offsetHeight,
+						offsetLeft: containerRef.current!.offsetLeft,
+						offsetTop: containerRef.current!.offsetTop,
 					},
 				})
+			}}
+			onMouseDown={(e) => {
+				if (!currIsActive) return
+				const { clientX: startX, clientY: startY } = e
+				const offsetLeft = containerRef.current!.offsetLeft
+				const offsetTop = containerRef.current!.offsetTop
+				let dragging = false
+				const oldSelectStart = document.onselectstart
+
+				document.onselectstart = () => false
+				document.addEventListener('mousemove', handleMouseMove)
+				document.addEventListener('mouseup', handleMouseUp)
+				document.addEventListener('mouseleave', handleMouseLeave)
+
+				function handleMouseMove(moveE: MouseEvent) {
+					if (!dragging) return
+					const { clientX, clientY } = moveE
+
+					const distX = clientX - startX
+					const distY = clientY - startY
+
+					containerRef.current!.style.left = `${offsetLeft + distX}px`
+					containerRef.current!.style.top = `${offsetTop + distY}px`
+				}
+
+				function handleMouseUp() {
+					if (!dragging) return
+					dragging = false
+					document.onselectstart = oldSelectStart
+					document.removeEventListener('mousemove', handleMouseMove)
+					document.removeEventListener('mouseup', handleMouseUp)
+					document.removeEventListener('mouseleave', handleMouseLeave)
+				}
+
+				function handleMouseLeave() {
+					if (!dragging) return
+					dragging = false
+					document.removeEventListener('mousemove', handleMouseMove)
+					document.removeEventListener('mouseup', handleMouseUp)
+					document.removeEventListener('mouseleave', handleMouseLeave)
+				}
 			}}
 		>
 			{children}
